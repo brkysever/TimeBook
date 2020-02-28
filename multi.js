0952b922f7831d6e7ada1fb996a1f1455e8f1299
brkysever <brkysever@gmail.com>
2020-02-28 15:20:04 +0300

    TimeBook

---
+++ multi.js
@@ -0,0 +1,8 @@
+function Carpma(){
+    const s1=Number(document.getElementById("sayi1").value);
+    const s2=Number(document.getElementById("sayi2").value);
+    const sonuc=s1*s2/24;
+    document.getElementById("kutuSonuc").textContent=Math.round(sonuc) + "gün";
+   
+}
+
